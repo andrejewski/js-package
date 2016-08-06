5813b0a86816a5a8ae16bc2771439106b433c772
Chris Andrejewski <christopher.andrejewski@gmail.com>
2016-08-05 20:26:39 -0400

    real initial commit

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# js-package
-npm/bower/browser boilerplate and setup scripts
+# JavaScript Package
+
+This is a starter repo for Node packages I publish, to start faster and with best practices and defaults ready to use.
+
+
+## Contributing
+
+Contributions are incredibly welcome as long as they are standardly applicable
+and pass the tests (or break bad ones). Tests are written in Mocha and
+assertions are done with the Node.js core `assert` module.
+
+```bash
+# running tests
+npm run test
+```
+
+Follow me on [Twitter](https://twitter.com/compooter) for updates or just for
+the lolz and please check out my other [repositories](https://github.com/andrejewski)
+ if I have earned it. I thank you for reading.
---
+++ bin/js-package.js
@@ -0,0 +1,27 @@
+var execSync = require('child_process').execSync;
+var pkg = require('../package');
+
+var args = process.argv.slice(2);
+var command = args[0];
+
+function log(message) {
+  process.stdout.write(message);
+}
+
+switch (command) {
+  case 'init':
+    var dir = args[1] || '.';
+    var url = pkg.git.repository.split('//').slice(1).join('//');
+    var gitUrl = 'git://' + url;
+    execSync('git clone --depth=1 --branch=master ' + gitUrl + ' ' +dir);
+    execSync('rm -rf ./.git && git init');
+    log('run `npm init` and `npm install` to finish setting up');
+    return;
+  default:
+    log([
+      pkg.name + ': ' + pkg.description,
+      '',
+      'Commands:',
+      '  init [dir]     clone js-package into [dir], defaults to current directory'
+    ].join('\n'));
+}
