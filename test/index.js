ae25649bd3dff3d04be81b0f19e1060400313656
Chris Andrejewski <christopher.andrejewski@gmail.com>
2016-11-21 17:07:20 -0500

    added code coverage

--- test/index.js
+++ test/index.js
@@ -1,10 +1,10 @@
 import test from 'ava'
-import leftPad from '../lib'
+import leftPad from '../src'
 
 test('should work', t => {
   t.is(leftPad('poop', 8), '    poop')
 })
 
-test('should not crash all of npm', t => {
-  t.truthy('meh')
+test('should use a given pad', t => {
+  t.is(leftPad('poop', 8, 'p'), 'pppppoop')
 })
